bb027440ff75d23d30e8decc430066d4eb14dfd6
Kushagra <kushagra.p495@gmail.com>
2024-05-13 21:35:11 +0530

    Added Custom Link Component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,16 +1,17 @@
 import prisma from "@/prisma/client";
-import {  Link, Table } from "@radix-ui/themes";
+import { Table } from "@radix-ui/themes";
 import React from "react";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from "delay";
 import IssuesToolbar from "./IssuesToolbar";
+import Link from "../components/Link";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
   await delay(2000);
   return (
     <div>
-     <IssuesToolbar />
+      <IssuesToolbar />
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
@@ -28,9 +29,7 @@ const IssuesPage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>
-                {issue.title}
-                </Link>
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
