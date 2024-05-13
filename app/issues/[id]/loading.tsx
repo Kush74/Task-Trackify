e635251c2ff6bf48225435ee25be5d94a72ca40c
Kushagra <kushagra.p495@gmail.com>
2024-05-13 22:12:10 +0530

    Added other loading skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { Heading, Flex, Card, Text, Skeleton, Box } from "@radix-ui/themes";
 
 const LoadingIsssueDetailsPage = () => {
   return (
-    <div>Loading...</div>
-  )
-}
+    <Box>
+      <Heading>
+        <Skeleton>Lorem ipsum dolor sit amet.</Skeleton>
+      </Heading>
+        <Flex gap="3" my="2" align="center">
+      <Skeleton >
+          <Text>Lorem</Text>
+      </Skeleton>
+      <Skeleton>
 
-export default LoadingIsssueDetailsPage
+          <Text>Lorem, ipsum.</Text>
+      </Skeleton>
+        </Flex>
+      <Card className="prose lg:prose-xl" mt="4">
+        <Text>
+          <Skeleton>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex
+            repudiandae tempore aliquid soluta et animi magni nam unde omnis
+            fuga dolorem, suscipit alias repellendus praesentium obcaecati
+            dolore in earum enim sed? Ad incidunt magnam aut eum deserunt quas
+            exercitationem neque officiis tenetur possimus tempore voluptatum
+            facere ipsum doloremque necessitatibus nulla, repudiandae enim
+            omnis? Nemo deleniti odit voluptatum sunt soluta consectetur earum
+            beatae autem commodi! Quia officia maxime quasi voluptatibus tempora
+            blanditiis velit pariatur nam! Facere, laborum tempore! Tempora,
+            facere delectus. At animi eum molestiae dolore vitae quisquam,
+            praesentium necessitatibus accusamus quidem aspernatur, excepturi
+            magnam amet tempora voluptatum voluptatibus sed ratione accusantium
+            sunt quam! Quae, quasi magni corporis recusandae quaerat alias.
+            Deleniti distinctio accusantium ex ab harum a dolore! Alias dolore
+            magni sapiente aliquam adipisci itaque corrupti cumque fuga debitis,
+            dignissimos doloremque quis eius saepe aspernatur reprehenderit
+            aliquid laborum corporis vel natus tenetur! Cupiditate, illum
+            debitis eaque at cumque laboriosam quos.
+          </Skeleton>
+        </Text>
+      </Card>
+    </Box>
+  );
+};
+
+export default LoadingIsssueDetailsPage;
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Skeleton} from "@radix-ui/themes";
+import React from "react";
 
 const LoadingNewIssuePage = () => {
   return (
-    <div>Loading...</div>
-  )
-}
+    <div className="max-w-xl">
+      <div className="space-y-3">
+        <Skeleton height={'2rem'}>
+          
+        </Skeleton>
+        <Skeleton height={'30rem'}></Skeleton>
+      </div>
+    </div>
+  );
+};
 
-export default LoadingNewIssuePage
+export default LoadingNewIssuePage;
