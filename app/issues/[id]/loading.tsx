6a78aa325e4e10c80467961853148fa752fa76f5
Kushagra <kushagra.p495@gmail.com>
2024-05-16 11:49:21 +0530

    Added Delete Issue Button

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,45 +1,66 @@
-import { Heading, Flex, Card, Text, Skeleton, Box } from "@radix-ui/themes";
+import {
+  Heading,
+  Flex,
+  Card,
+  Text,
+  Skeleton,
+  Box,
+  Grid,
+  Button,
+} from "@radix-ui/themes";
 
 const LoadingIsssueDetailsPage = () => {
   return (
-    <Box>
-      <Heading>
-        <Skeleton>Lorem ipsum dolor sit amet.</Skeleton>
-      </Heading>
+    <Grid columns={{ initial: "1", md: "5" }} gap="5">
+      <Box className="col-span-4">
+        <Heading>
+          <Skeleton>Lorem ipsum dolor sit amet. </Skeleton>
+        </Heading>
         <Flex gap="3" my="2" align="center">
-      <Skeleton >
-          <Text>Lorem</Text>
-      </Skeleton>
-      <Skeleton>
-
-          <Text>Lorem, ipsum.</Text>
-      </Skeleton>
-        </Flex>
-      <Card className="prose lg:prose-xl" mt="4">
-        <Text>
           <Skeleton>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex
-            repudiandae tempore aliquid soluta et animi magni nam unde omnis
-            fuga dolorem, suscipit alias repellendus praesentium obcaecati
-            dolore in earum enim sed? Ad incidunt magnam aut eum deserunt quas
-            exercitationem neque officiis tenetur possimus tempore voluptatum
-            facere ipsum doloremque necessitatibus nulla, repudiandae enim
-            omnis? Nemo deleniti odit voluptatum sunt soluta consectetur earum
-            beatae autem commodi! Quia officia maxime quasi voluptatibus tempora
-            blanditiis velit pariatur nam! Facere, laborum tempore! Tempora,
-            facere delectus. At animi eum molestiae dolore vitae quisquam,
-            praesentium necessitatibus accusamus quidem aspernatur, excepturi
-            magnam amet tempora voluptatum voluptatibus sed ratione accusantium
-            sunt quam! Quae, quasi magni corporis recusandae quaerat alias.
-            Deleniti distinctio accusantium ex ab harum a dolore! Alias dolore
-            magni sapiente aliquam adipisci itaque corrupti cumque fuga debitis,
-            dignissimos doloremque quis eius saepe aspernatur reprehenderit
-            aliquid laborum corporis vel natus tenetur! Cupiditate, illum
-            debitis eaque at cumque laboriosam quos.
+            <Text>Lorem</Text>
           </Skeleton>
-        </Text>
-      </Card>
-    </Box>
+          <Skeleton>
+            <Text>Lorem, ipsum.</Text>
+          </Skeleton>
+        </Flex>
+        <Card className="prose lg:prose-xl max-w-full" mt="4">
+          <Text>
+            <Skeleton>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex
+              repudiandae tempore aliquid soluta et animi magni nam unde omnis
+              fuga dolorem, suscipit alias repellendus praesentium obcaecati
+              dolore in earum enim sed? Ad incidunt magnam aut eum deserunt quas
+              exercitationem neque officiis tenetur possimus tempore voluptatum
+              facere ipsum doloremque necessitatibus nulla, repudiandae enim
+              omnis? Nemo deleniti odit voluptatum sunt soluta consectetur earum
+              beatae autem commodi! Quia officia maxime quasi voluptatibus
+              tempora blanditiis velit pariatur nam! Facere, laborum tempore!
+              Tempora, facere delectus. At animi eum molestiae dolore vitae
+              quisquam, praesentium necessitatibus accusamus quidem aspernatur,
+              excepturi magnam amet tempora voluptatum voluptatibus sed ratione
+              accusantium sunt quam! Quae, quasi magni corporis recusandae
+              quaerat alias. Deleniti distinctio accusantium ex ab harum a
+              dolore! Alias dolore magni sapiente aliquam adipisci itaque
+              corrupti cumque fuga debitis, dignissimos doloremque quis eius
+              saepe aspernatur reprehenderit aliquid laborum corporis vel natus
+              tenetur! Cupiditate, illum debitis eaque at cumque laboriosam
+              quos.
+            </Skeleton>
+          </Text>
+        </Card>
+      </Box>
+      <Box>
+        <Flex direction={"column"} gap={"4"}>
+          <Skeleton>
+            <Button>Lorem, ipsum.</Button>
+          </Skeleton>
+          <Skeleton>
+            <Button>Lorem, ipsum.</Button>
+          </Skeleton>
+        </Flex>
+      </Box>
+    </Grid>
   );
 };
 
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,10 @@
 import prisma from "@/prisma/client";
-import { Box, Grid } from "@radix-ui/themes";
+import { Box, Flex, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
+import DeleteIssueButton from "./DeleteIssueButton";
+import delay from "delay";
 
 interface Props {
   params: { id: string };
@@ -15,13 +17,17 @@ const IssueDetailPage = async ({ params }: Props) => {
 
   if (!issue) notFound();
 
+  await delay(5000)
   return (
-    <Grid columns={{ initial: "1", md: "2" }} gap="5">
-      <Box>
-        <IssueDetails issue={issue}/> 
+    <Grid columns={{ initial: "1", sm: "5" }} gap="5">
+      <Box className="md:col-span-4">
+        <IssueDetails issue={issue} />
       </Box>
       <Box>
-        <EditIssueButton issueId={issue.id} />
+        <Flex direction={"column"} gap={"4"}>
+          <EditIssueButton issueId={issue.id} />
+          <DeleteIssueButton issueId={issue.id} />
+        </Flex>
       </Box>
     </Grid>
   );
